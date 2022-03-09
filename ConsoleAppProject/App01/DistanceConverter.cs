6bdd09858c3111ae098cc04c8849633584e63144
Nathaniel Meer <22139079@bucks.ac.uk>
2022-03-09 09:21:06 +0000

    Added empty methods and attributes

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -3,12 +3,44 @@
 namespace ConsoleAppProject.App01
 {
     /// <summary>
-    /// Please describe the main features of this App
+    /// This class offers methods for converting a given distance
+    /// measured in miles to the equivalent distance measured in feet
     /// </summary>
     /// <author>
-    /// Derek version 0.1
+    /// Nathaniel Meer | Version: 0.1
     /// </author>
     public class DistanceConverter
     {
+        private double miles;
+        private double feet;
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Run()
+        {
+
+        }
+        /// <summary>
+        /// Promt the user to enter the distance in miles
+        /// Input the miles as a double number
+        /// </summary>
+        private void InputMiles()
+        {
+
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        private void CalculateFeet()
+        {
+
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        private void OutputFeet()
+        {
+
+        }
     }
 }
